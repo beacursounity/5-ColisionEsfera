1e11cd2849c864dc4ff6dd77e001555ea6fa7a2b
beacursounity <beacursounity@gmail.com>
2018-12-20 10:15:01 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Esfera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esfera : MonoBehaviour {
+
+    [SerializeField] ParticleSystem explosion;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ha entrado");
+        explosion.Play();
+    }
+}
